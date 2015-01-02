3590833d5723cec8cc9b87afa50ded32eba84d76
System Administrator <root@ololos-iMac.local>
2015-01-02 15:57:32 +0300

    Make it so

---
+++ client/leaderboard.js
@@ -0,0 +1,62 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+/*Players = new Mongo.Collection("players2");
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    players: function () {
+      return Players.find({}, { sort: { score: -1, name: 1 } });
+    },
+    selectedName: function () {
+      var player = Players.findOne(Session.get("selectedPlayer"));
+      return player && player.name;
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .inc': function () {
+      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
+    }
+  });
+
+  Template.player.helpers({
+    selected: function () {
+      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selectedPlayer", this._id);
+    }
+  });
+}
+
+// On server startup, create some players if the database is empty.
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Players.find().count() === 0) {
+      var names = ["1", "Grace", "Curie",
+                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+      _.each(names, function (name) {
+        Players.insert({
+          name: name,
+          score: Math.floor(Random.fraction() * 10) * 5
+        });
+      });
+    }
+  });
+} */
+
+/*
+(function(e){
+  jQuery(document).textmistake({
+    'mailTo': 'your_email(d0g)here.com'.replace(/\(d0g\)/, '@'),
+    'mailFrom': 'textmistake(d0g)yourdomain.com'.replace(/\(d0g\)/, '@'),
+    'mandrillKey': 'HereIsYourMandrillApiKey', // Get your - https://mandrill.com/signup/
+  });
+})(jQuery)
+
+*/
+
